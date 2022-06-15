ba0c5197cd641d4c23bd61ab8e380fef24ea5f95
Pratyush <space4pratyush@gmail.com>
2022-06-15 12:52:05 +0530

    created template driven form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,26 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'template-driven-form';
+
+  countryList:Country[]=[
+    new Country("1", "India"),
+    new Country("2", "USA"),
+    new Country("3", "England")
+  ];
+
+  onSubmit(contactForm, pincode){
+    console.log(contactForm.value);
+    console.log(pincode.value);
+  }
+
+}
+
+export class Country{
+  id:string;
+  name:string;
+
+  constructor(id:string, name:string){
+    this.id=id;
+    this.name=name;
+  }
 }
